77feeacac5cb5057de769fb50372dca932ec5434
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-14 14:30:00 +0600

    cloudanay service fiel complite

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,16 +2,22 @@ function getConfig(): {
   database: { uri?: string };
   port: number;
   jwt: { secret?: string; expires: string };
+  cloudinary: { cloudName?: string; apiKey?: string; apiSecret?: string };
 } {
   return {
     database: {
       uri: process.env.MONGODB_URI,
     },
     port: process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000,
-    // JWT কনফিগ এখানে যোগ করুন
+    // JWT
     jwt: {
       secret: process.env.JWT_SECRET,
       expires: process.env.JWT_EXPIRES || '1d',
+    },
+    cloudinary: {
+      cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+      apiKey: process.env.CLOUDINARY_API_KEY,
+      apiSecret: process.env.CLOUDINARY_API_SECRET,
     },
   };
 }
