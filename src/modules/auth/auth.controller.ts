f5da165d03d94e91a75a61acff99e6dee1277493
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-14 15:27:07 +0600

    auth controller file create

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,42 +1,33 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  UseInterceptors,
+  UploadedFile,
+  Ip,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) { }
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('register')
+  @UseInterceptors(FileInterceptor('image'))
+  async register(
+    @Body() createAuthDto: CreateAuthDto,
+    @Ip() ip: string,
+    @UploadedFile() file?: Express.Multer.File,
+  ): Promise<unknown> {
+    let uploadedImageUrl = '';
+    if (file) {
+      uploadedImageUrl = await this.cloudinaryService.uploadFile(file, 'users');
+    }
+    return this.authService.register(createAuthDto, ip, uploadedImageUrl);
   }
 }
