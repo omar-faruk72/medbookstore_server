4925d6ab09ef3dc4096ae298b9300d8e7a599bd8
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-14 14:39:56 +0600

    create auth dto file

--- src/modules/auth/dto/create-auth.dto.ts
+++ src/modules/auth/dto/create-auth.dto.ts
@@ -1,1 +1,30 @@
-export class CreateAuthDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateAuthDto {
+  @IsNotEmpty({ message: 'নাম দেওয়া বাধ্যতামূলক' })
+  @IsString({ message: 'নাম অবশ্যই একটি স্ট্রিং হতে হবে' })
+  name!: string;
+
+  @IsNotEmpty({ message: 'ইমেইল দেওয়া বাধ্যতামূলক' })
+  @IsEmail({}, { message: 'সঠিক ইমেইল এড্রেস দিন' })
+  email!: string;
+
+  @IsNotEmpty({ message: 'পাসওয়ার্ড দেওয়া বাধ্যতামূলক' })
+  @MinLength(6, { message: 'পাসওয়ার্ড অন্তত ৬ অক্ষরের হতে হবে' })
+  @IsString({ message: 'পাসওয়ার্ড অবশ্যই একটি স্ট্রিং হতে হবে' })
+  password!: string;
+
+  @IsOptional()
+  @IsString({ message: 'ইমেজ ইউআরএল অবশ্যই একটি স্ট্রিং হতে হবে' })
+  imageUrl?: string;
+
+  @IsOptional({ message: 'ঠিকানা দেওয়া বাধ্যতামূলক' })
+  @IsString({ message: 'ঠিকানা অবশ্যই একটি স্ট্রিং হতে হবে' })
+  address!: string;
+}
