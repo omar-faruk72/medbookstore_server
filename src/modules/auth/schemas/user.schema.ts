68b214e6cfc085634459f9d00ca69ca5e8dbc09a
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-14 15:22:54 +0600

    user schema update

--- src/modules/auth/schemas/user.schema.ts
+++ src/modules/auth/schemas/user.schema.ts
@@ -3,26 +3,26 @@ import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class User extends Document {
-    @Prop({ required: true, trim: true })
-    name!: string;
+  @Prop({ required: true, trim: true })
+  name!: string;
 
-    @Prop({ required: true, unique: true, trim: true, lowercase: true })
-    email!: string;
+  @Prop({ required: true, unique: true, trim: true, lowercase: true })
+  email!: string;
 
-    @Prop({ required: false })
-    password?: string;
+  @Prop({ required: false }) 
+  password?: string;
 
-    @Prop({ required: false, default: '' })
-    imageUrl?: string;
+  @Prop({ required: false, default: '' })
+  imageUrl?: string;
 
-    @Prop({ required: false })
-    address?: string;
+  @Prop({ required: false, default: '' })
+  address?: string;
 
-    @Prop({ type: [String], default: [] })
-    ipAddresses!: string[];
+  @Prop({ type: [String], default: [] }) 
+  ipAddresses!: string[];
 
-    @Prop({ required: false, default: 'credentials' }) 
-    provider?: string;
+  @Prop({ required: false, default: 'credentials' })
+  provider?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
