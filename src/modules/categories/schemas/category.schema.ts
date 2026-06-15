91ed5a4c4628254f53a645cb93c603a2e3297bb1
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-15 14:42:49 +0600

    category schema file code incloudes

---
+++ src/modules/categories/schemas/category.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Category extends Document {
+  @Prop({ required: true, unique: true, trim: true })
+  name: string;
+
+  @Prop({ trim: true, default: '' })
+  description: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
