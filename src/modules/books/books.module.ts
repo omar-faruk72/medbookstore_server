d350c4e42a0b320efcc3480f37e4e20e2f289d0e
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-15 16:50:05 +0600

    boks module file code incloudes

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { Book, BookSchema } from './schemas/book.schema';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module'; 
+import { AuthModule } from '../auth/auth.module'; 
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
+    CloudinaryModule,
+    AuthModule,
+  ],
   controllers: [BooksController],
   providers: [BooksService],
 })
