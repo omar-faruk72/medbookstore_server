f5b5b3d830c1729f6bf4169990d85e7ceabd7afc
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-15 14:56:01 +0600

    create category dto file code added

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty({ message: 'ক্যাটাগরির নাম দেওয়া আবশ্যক!' })
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
