d6cbae3383f2e41a5e77d206a47104aae8e4e250
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-15 14:57:21 +0600

    categories modules file code update

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { Category, CategorySchema } from './schemas/category.schema';
+import { AuthModule } from '../auth/auth.module'; 
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }]),
+    AuthModule, 
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
