8f095b374c36f2823bd4a208d5c9d90e28a7f835
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-15 16:48:56 +0600

    create book dto file code incloudes

--- src/modules/books/dto/create-book.dto.ts
+++ src/modules/books/dto/create-book.dto.ts
@@ -1,1 +1,27 @@
-export class CreateBookDto {}
+import { IsNotEmpty, IsString, IsNumberString, IsOptional, IsEnum } from 'class-validator';
+import { PublishStatus } from '../schemas/book.schema';
+
+export class CreateBookDataDto {
+  @IsString()
+  @IsNotEmpty({ message: 'বইয়ের নাম দেওয়া আবশ্যক!' })
+  name: string;
+
+  @IsNumberString({}, { message: 'বইয়ের দাম অবশ্যই একটি সংখ্যা হতে হবে!' })
+  price: string; 
+
+  @IsString()
+  @IsNotEmpty({ message: 'ক্যাটাগরি আইডি দেওয়া আবশ্যক!' })
+  category: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'বইয়ের ডিটেইলস দেওয়া আবশ্যক!' })
+  details: string;
+
+  @IsOptional()
+  @IsString()
+  specialFeatures?: string;
+
+  @IsOptional()
+  @IsEnum(PublishStatus, { message: 'স্ট্যাটাস অবশ্যই active অথবা draft হতে হবে' })
+  publishStatus?: PublishStatus;
+}
