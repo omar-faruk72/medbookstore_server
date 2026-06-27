29178b49df3929314bf6447f1362673e1e3739af
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-27 18:50:40 +0600

    create an pdfs collections

--- src/modules/pdfs/pdfs.module.ts
+++ src/modules/pdfs/pdfs.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PdfsService } from './pdfs.service';
 import { PdfsController } from './pdfs.controller';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module'; 
+import { AuthModule } from '../auth/auth.module'; 
+import { Pdf, PdfSchema } from './schema/pdf.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Pdf.name, schema: PdfSchema }]),
+    CloudinaryModule,
+    AuthModule,
+  ],
   controllers: [PdfsController],
   providers: [PdfsService],
 })
