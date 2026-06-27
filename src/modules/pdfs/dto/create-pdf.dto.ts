6c413a707e10a36de3f9f81c3a8542004a2d3f71
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-06-27 18:45:45 +0600

    create an pdfs collections

--- src/modules/pdfs/dto/create-pdf.dto.ts
+++ src/modules/pdfs/dto/create-pdf.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePdfDto {}
+import { IsNotEmpty, IsString, IsNumberString, IsOptional } from 'class-validator';
+
+export class CreatePdfDataDto {
+  @IsString()
+  @IsNotEmpty({ message: 'পিডিএফ এর নাম দেওয়া আবশ্যক!' })
+  name!: string;
+
+  @IsNumberString({}, { message: 'পিডিএফ এর দাম অবশ্যই একটি সংখ্যা হতে হবে!' })
+  price!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'পিডিএফ এর ডিটেইলস দেওয়া আবশ্যক!' })
+  details!: string;
+
+  @IsOptional()
+  @IsString()
+  specialFeatures?: string;
+}
