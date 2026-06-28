b296b9b1fe11a8c57400cb9af1f75b75cc3f73dd
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-06-28 21:15:22 +0600

    socail login

--- src/modules/auth/dto/create-auth.dto.ts
+++ src/modules/auth/dto/create-auth.dto.ts
@@ -1,24 +1,24 @@
 import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateAuthDto {
-  @IsNotEmpty({ message: 'নাম দেওয়া বাধ্যতামূলক' })
+  @IsNotEmpty({ message: 'নাম দেওয়া বাধ্যতামূলক' })
   @IsString({ message: 'নাম অবশ্যই স্ট্রিং হতে হবে' })
   name!: string;
 
-  @IsNotEmpty({ message: 'ইমেইল দেওয়া বাধ্যতামূলক' })
+  @IsNotEmpty({ message: 'ইমেইল দেওয়া বাধ্যতামূলক' })
   @IsEmail({}, { message: 'সঠিক ইমেইল অ্যাড্রেস দিন' })
   email!: string;
 
-  @IsNotEmpty({ message: 'পাসওয়ার্ড দেওয়া বাধ্যতামূলক' })
-  @MinLength(6, { message: 'পাসওয়ার্ড অন্তত ৬ অক্ষরের হতে হবে' })
-  @IsString({ message: 'পাসওয়ার্ড অবশ্যই স্ট্রিং হতে হবে' })
+  @IsNotEmpty({ message: 'পাসওয়ার্ড দেওয়া বাধ্যতামূলক' })
+  @MinLength(6, { message: 'পাসওয়ার্ড অন্তত ৬ অক্ষরের হতে হবে' })
+  @IsString({ message: 'পাসওয়ার্ড অবশ্যই স্ট্রিং হতে হবে' })
   password!: string;
 
-  @IsOptional() // 👈 ইউজার না দিলেও কোনো সমস্যা নেই
-  @IsString({ message: 'ইমেজ ইউআরএল স্ট্রিং হতে হবে' })
+  @IsOptional()
+  @IsString({ message: 'ইメージ ইউআরএল স্ট্রিং হতে হবে' })
   imageUrl?: string;
 
-  @IsOptional() // 👈 অ্যাড্রেসও এখন অপশনাল
+  @IsOptional()
   @IsString({ message: 'ঠিকানা অবশ্যই স্ট্রিং হতে হবে' })
   address?: string;
 }
