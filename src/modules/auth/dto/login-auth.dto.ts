80d77138373a57eeafe2a2441e48641ee648038b
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-06-28 21:16:51 +0600

    socail login

--- src/modules/auth/dto/login-auth.dto.ts
+++ src/modules/auth/dto/login-auth.dto.ts
@@ -2,10 +2,10 @@ import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginAuthDto {
   @IsEmail({}, { message: 'সঠিক ইমেইল অ্যাড্রেস দিন' })
-  @IsNotEmpty({ message: 'ইমেইল দেওয়া বাধ্যতামূলক' })
+  @IsNotEmpty({ message: 'ইমেইল দেওয়া বাধ্যতামূলক' })
   email!: string;
 
-  @IsNotEmpty({ message: 'পাসওয়ার্ড দেওয়া বাধ্যতামূলক' })
-  @MinLength(6, { message: 'পাসওয়ার্ড অন্তত ৬ অক্ষরের হতে হবে' })
+  @IsNotEmpty({ message: 'পাসওয়ার্ড দেওয়া বাধ্যতামূলক' })
+  @MinLength(6, { message: 'পাসওয়ার্ড অন্তত ৬ অক্ষরের হতে হবে' })
   password!: string;
 }
